d055e1e09d00730ff78c25b616fc5f27d53e25af
Dyneteve <sound0020@gmail.com>
2018-03-16 13:14:28 -0400

    Add lineage-15.1 repopicks
    Change-Id: I6020f3ab032d199d96d31d24acdd4c194dea6150

--- repopicks-15.1.sh
+++ repopicks-15.1.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+### RepoPick list for samsung-msm8916 ###
+. build/envsetup.sh
+
+## Camera ##
+repopick -f -r 206130
+repopick -f -r 206861
+repopick -f -r 206433
+repopick -f -r 206435
+repopick -f -r 206968
+repopick -f -r 206970
+repopick -f -r 206969
+repopick -f -r 206430
+repopick -f -r 206434
+repopick -f -r 206431
+repopick -f -r 206432
