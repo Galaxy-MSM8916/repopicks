1c249b2551845b7872a6d0841bf650c9af8a131e
Dyneteve <sound0020@gmail.com>
2018-03-24 15:39:54 -0400

    Remove all repopicks
    Change-Id: I8b444fc4daa79327e4da28757af3bff23ca518c2

--- repopicks-15.1.sh
+++ repopicks-15.1.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-### RepoPick list for samsung-msm8916 ###
-. build/envsetup.sh
-
-## Camera ##
-repopick -f -r 206130
-repopick -f -r 206861
-repopick -f -r 206433
-repopick -f -r 206435
-repopick -f -r 206968
-repopick -f -r 206970
-repopick -f -r 206969
-repopick -f -r 206430
-repopick -f -r 206431
-repopick -f -r 206432
+true
