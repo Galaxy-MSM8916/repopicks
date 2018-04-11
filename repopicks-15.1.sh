b1952366500009f270ba0a251e6758d739806eba
Dyneteve <sound0020@gmail.com>
2018-04-11 07:39:35 -0400

    Add more picks
    Change-Id: I33a8d9db088b7ae94c9de69228d29dedc5e344dd

--- repopicks-15.1.sh
+++ repopicks-15.1.sh
@@ -5,3 +5,13 @@
 
 ## Camera ##
 repopick -f -r 209883
+repopick -f -r 206969
+repopick -f -r 206968
+repopick -f -r 206435
+repopick -f -r 206434
+repopick -f -r 206433
+repopick -f -r 206432
+repopick -f -r 206431
+repopick -f -r 206430
+repopick -f -r 206970
+repopick -f -r 206116
