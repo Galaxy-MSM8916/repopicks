2fecf86be86d2a500f67f818770cb7cc4e59d16b
Dyneteve <sound0020@gmail.com>
2018-04-21 09:25:35 -0400

    Clean up repopicks
    Change-Id: I76f24d377f1ca98e1af71a94e55612d81c8b36a5

--- repopicks-15.1.sh
+++ repopicks-15.1.sh
@@ -4,14 +4,18 @@
 . build/envsetup.sh
 
 ## Camera ##
-repopick -f -r 209883
-repopick -f -r 206969
-repopick -f -r 206968
-repopick -f -r 206435
 repopick -f -r 206434
-repopick -f -r 206433
-repopick -f -r 206432
-repopick -f -r 206431
-repopick -f -r 206430
+repopick -f -r 206968
 repopick -f -r 206970
-repopick -f -r 206116
+repopick -f -r 206969
+repopick -f -r 206433
+repopick -f -r 206431
+repopick -f -r 206435
+
+# Camera Hal 1 ##
+repopick -f -r 206426
+repopick -f -r 206428
+repopick -f -r 206429
+repopick -f -r 206427
+repopick -f -r 213062
+
