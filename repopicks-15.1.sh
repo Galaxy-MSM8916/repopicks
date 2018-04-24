499d08bba5142c6d343bb6c4b20c3c717486d41a
Dyneteve <sound0020@gmail.com>
2018-04-24 13:47:47 -0400

    Clean up repopicks
    * Some of them have been merged
    
    Change-Id: Ie19b0a1ea53797be0b92e4eb21e8beb05a8372c2

--- repopicks-15.1.sh
+++ repopicks-15.1.sh
@@ -5,17 +5,5 @@
 
 ## Camera ##
 repopick -f -r 206434
-repopick -f -r 206968
-repopick -f -r 206970
-repopick -f -r 206969
-repopick -f -r 206433
-repopick -f -r 206431
-repopick -f -r 206435
-
-# Camera Hal 1 ##
-repopick -f -r 206426
-repopick -f -r 206428
-repopick -f -r 206429
-repopick -f -r 206427
 repopick -f -r 213062
 
